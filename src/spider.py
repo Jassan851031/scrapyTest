037b911b6c04b79d3b2547a5a75b408a603d2dfb
Jassan Leyva <jassan851031@gmail.com>
2021-04-27 15:44:14 -0400

    Accediendo a la opcion Listado

--- src/spider.py
+++ src/spider.py
@@ -1,4 +1,16 @@
 import selenium
 from selenium import webdriver
 
-browser = webdriver.Chrome(executable_path="/home/jorge/Documentos/interviews/testScrapy/src/chromedriver_binary")
+
+url = 'https://www.enquefase.cl/'
+
+# abriendo el browser
+driver = webdriver.Chrome(executable_path="/home/jorge/Documentos/interviews/testScrapy/src/chromedriver_binary")
+
+#obteniendo url
+driver.get(url)
+
+# abriendo menu Listado de la izquierda
+listado = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/ul/li[3]/a")
+listado.click()
+
