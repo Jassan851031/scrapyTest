3298face0ab552a40bad956a6e91219d18b47c1c
Jassan Leyva <jassan851031@gmail.com>
2021-04-29 01:38:46 -0400

    eliminando lineas innecesarias

--- src/db_config.py
+++ src/db_config.py
@@ -1,9 +1,5 @@
 from pymongo import MongoClient
 
-# URL = "mongodb://localhost:8080"
-# USER = "admin"
-# PASS = "admin1234"
-
 def insertData(arr):
 
     client = MongoClient("mongodb+srv://admin:admin1234@myCluster.kqvdy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
